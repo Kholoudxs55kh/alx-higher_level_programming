16f05376e22865247c4e96eb1cdc540bea14e6c1
Kholoudxs55kh <kfattem@gmail.com>
2023-06-05 17:02:31 -0700

    ay7aga

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * check_cycle - .
+ * @list: .
+ * Return: .
+ */
+int check_cycle(listint_t *list)
+{
+	while (list->next)
+		return (1);
+	return (0);
+	/*
+	 * anything
+	 * /
+}
