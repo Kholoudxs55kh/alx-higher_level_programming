a434fc43dc9523b2af03ec627b560fd11237f5b8
Kholoudxs55kh <kfattem@gmail.com>
2023-06-06 09:35:02 -0700

    a33333333

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for k in range(0, 10):
+    for h in range(k + 1, 10):
+        if k != h:
+            if k != 8 or h != 9:
+                 print("{:d}".format(k) + "{:d}, ".format(h), end ="")
+print(89)
