4c3c4813ae8ae596408a1177b349124c2cb6ba82
Kholoudxs55kh <kfattem@gmail.com>
2023-06-08 15:31:09 -0700

    yllatry

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,10 @@ if __name__ == "__main__":
     argc = len(sys.argv) - 1
     if argc == 0:
         print("0 arguments.")
-    elif argc >= 1:
+    elif argc == 1:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
         print("{} arguments:".format(argc))
-        for i in range(argc):
-            print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
+        for i, arg in enumerate(sys.argv[1:], start=1):
+            print("{}: {}".format(i, arg))
--- 0x02-python-import_modules/2i.py
+++ 0x02-python-import_modules/2i.py
@@ -8,3 +8,13 @@ if __name__ == "__main__":
         print("{} arguments:".format(argc))
         for i, arg in enumerate(sys.argv[1:], start=1):
             print("{}: {}".format(i, arg))
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc >= 1:
+        print("{} arguments:".format(argc))
+        for i in range(argc):
+            print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
