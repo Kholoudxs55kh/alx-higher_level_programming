0b79f60a0432c72f02bc9a8c22acb523fe2b2c3e
Kholoudxs55kh <kfattem@gmail.com>
2023-06-08 15:20:29 -0700

    Task3!

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(argc))
+        for i, arg in enumerate(sys.argv[1:], start=1):
+            print("{}: {}".format(i, arg))
