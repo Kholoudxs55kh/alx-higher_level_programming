e05e14acfa65880910f5e2ede3fa2ac5b585109a
Kholoudxs55kh <kfattem@gmail.com>
2023-06-08 15:47:57 -0700

    task4

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argc = sys.argv[1:]
     result = 0
+    argc = sys.argv[1:]
     for arg in argc:
         result += int(arg)
     print(result)
