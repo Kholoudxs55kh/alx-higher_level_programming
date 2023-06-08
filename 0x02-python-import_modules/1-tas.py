09cfbefb631f7b3bb41ca011b184bd9f6aced462
Kholoudxs55kh <kfattem@gmail.com>
2023-06-08 09:23:25 -0700

    Task2

--- 0x02-python-import_modules/1-tas.py
+++ 0x02-python-import_modules/1-tas.py
@@ -2,12 +2,12 @@
 if __name__ == "__main__":
     a = 10
     b = 5
-    from calculator_1 import add, sub, mul, div
-    result = add(a, b)
+    from calculator_1 import add
+    result = a + b
     print("{:d} + {:d} = {:d}".format(a, b, result))
-    result = sub(a, b)
+    result = a - b
     print("{:d} - {:d} = {:d}".format(a, b, result))
-    result = mul(a, b)
+    result = a * b
     print("{:d} * {:d} = {:d}".format(a, b, result))
-    result = div(a, b)
+    result = a / b
     print("{:d} / {:d} = {:d}".format(a, b, int(result)))
