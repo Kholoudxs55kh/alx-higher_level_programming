b88ab8fbc1b7a9b66f3f17f08ea1e31a64c3480d
Kholoudxs55kh <kfattem@gmail.com>
2023-06-08 15:29:25 -0700

    Task3

--- 0x02-python-import_modules/2i.py
+++ 0x02-python-import_modules/2i.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif argc >= 1:
         print("{} arguments:".format(argc))
-        for i in range(argc):
-            print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
+        for i, arg in enumerate(sys.argv[1:], start=1):
+            print("{}: {}".format(i, arg))
