4ab910da124d317771435e938b97b9ace68de155
Kholoudxs55kh <kfattem@gmail.com>
2023-06-08 15:23:49 -0700

    Task3!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     argc = len(sys.argv) - 1
     if argc == 0:
         print("0 arguments.")
-    else:
+    elif argc >= 1:
         print("{} arguments:".format(argc))
         for i, arg in enumerate(sys.argv[1:], start=1):
             print("{}: {}".format(i, arg))
