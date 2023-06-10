91e67c2f513732f86e41c143797dcc956ec91ecc
Kholoudxs55kh <kfattem@gmail.com>
2023-06-10 14:30:59 -0700

    Testing?

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    _tuple = (len(sentence), sentence[0] if sentence is not None else None)
+    _tuple = (len(sentence) if sentence else 0,
+              sentence[0] if sentence is not None else None)
     return _tuple
