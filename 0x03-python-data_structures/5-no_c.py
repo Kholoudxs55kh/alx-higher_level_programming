926889e95a00c1a1b024d85c8cf7b4f8a44425bc
Kholoudxs55kh <kfattem@gmail.com>
2023-06-10 14:21:38 -0700

    Testing?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    #    return my_string.replace('c', '').replace('C', '')
-    #    not allowe y a3ma y a3maaaa :DDD
     _string = ""
     for i in my_string:
         if i != 'c' and i != 'C':
