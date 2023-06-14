42cb2734ede7633ef5a42946298363e92913bc23
Kholoudxs55kh <kfattem@gmail.com>
2023-06-14 15:21:11 -0700

    Task1

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    #   listt = []
+    #   new_matrix = listt + matrix
+    #   for D1 in new_matrix:
+    #       for D2 in range(len(D1)):
+    #            D1[D2] **= 2
+    #    for D1 in matrix:
+    #        for D2 in D1:
+    #            mat_ = []
+    #            mat_ += D2**2
+    #    return mat_
+    mat_ = []
+    for D1 in matrix:
+        listt = []
+        for D2 in D1:
+            listt.append(D2**2)
+        mat_.append(listt)
+    return mat_
