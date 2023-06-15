7fc66f8a53632f12de9e4c4c955ea2c9ed10fb9a
Kholoudxs55kh <kfattem@gmail.com>
2023-06-14 17:34:40 -0700

    EightthTask

--- 0x04-python-more_data_structures/7.py
+++ 0x04-python-more_data_structures/7.py
@@ -4,6 +4,7 @@ print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_d
 
 a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
 new_dict = update_dictionary(a_dictionary, 'language', "Python")
+print_sorted_dictionary(new_dict)
 print("--")
 print_sorted_dictionary(a_dictionary)
 
@@ -11,6 +12,7 @@ print("--")
 print("--")
 
 new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
+print_sorted_dictionary(new_dict)
 print("--")
 print_sorted_dictionary(a_dictionary)
 
