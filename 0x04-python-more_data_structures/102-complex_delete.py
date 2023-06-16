5798dbf7e4817bbf6a1c82e10789944f86433dbe
Kholoudxs55kh <kfattem@gmail.com>
2023-06-16 16:30:21 -0700

    Et3amel

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    to_del = []
+    for key in a_dictionary:
+        if a_dictionary[key] == value:
+            to_del.append(key)
+    for i in to_del:
+        del a_dictionary[i]
