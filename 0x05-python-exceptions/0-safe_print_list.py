0f9c4bc26a0f08cc0a17bb2ff6e72592b598a25d
Kholoudxs55kh <kfattem@gmail.com>
2023-06-26 14:19:17 -0700

    FixedTaskOne

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -16,7 +16,7 @@ def safe_print_list(my_list=[], x=0):
             for ele in range(0, x):
                 print("{}".format(my_list[ele]), end="")
                 elements += 1
-    except:
+    except IndexError:
         pass
     print()
 
