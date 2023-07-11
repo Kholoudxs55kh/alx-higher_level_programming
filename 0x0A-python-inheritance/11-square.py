7761b9a3eee8f2051c424d16b4d93f0ae7ee1b11
Kholoudxs55kh <kfattem@gmail.com>
2023-07-11 00:03:25 -0700

    LastMandatoryy

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+task 11
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """square"""
+
+    def __init__(self, size):
+        """instantiation of the square"""
+
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """"returns the area of the square"""
+
+        return self.__size ** 2
+
+    def __str__(self):
+        """string representation of the square"""
+
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
