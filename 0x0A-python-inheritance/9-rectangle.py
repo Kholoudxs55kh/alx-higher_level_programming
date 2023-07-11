ae6dcfd9960e68fff59cfa55bc69ac37df3fe45f
Kholoudxs55kh <kfattem@gmail.com>
2023-07-10 23:11:50 -0700

    task10

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Contains the class BaseGeometry and subclass Rectangle
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """rectangle instantiation"""
+
+    def __init__(self, width, height):
+        """rectangle instantiation"""
+
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """informal string representation of the rectangle"""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
