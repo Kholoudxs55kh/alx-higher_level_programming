7ad2822949a45967a2816fcdbdb4f65ee60a7fe8
Kholoudxs55kh <kfattem@gmail.com>
2023-07-17 14:49:07 -0700

    task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -37,3 +37,11 @@ class Base:
                 my_list.append(i.to_dictionary())
         with open(filename, mode='w') as filee:
             filee.write(cls.to_json_string(my_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns List of the JSON string representation"""
+
+        if json_string is None:
+            return []
+        return json.loads(json_string)
