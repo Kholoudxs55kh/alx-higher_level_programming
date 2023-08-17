d09d608b179862238f03ad19954e88946c0ad60a
Kholoudxs55kh <kfattem@gmail.com>
2023-08-16 18:55:04 -0700

    task 10-16 , many joins

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,3 @@
+-- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+SELECT tv_genres.name AS genre, COUNT(tv_genres.name) AS number_of_shows FROM tv_genres, tv_show_genres WHERE tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,3 @@
+-- script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
+SELECT tv_shows.title FROM tv_shows, tv_show_genres, tv_genres WHERE tv_shows.id = tv_show_genres.show_id and tv_show_genres.genre_id = tv_genres.id and
+tv_shows.title = "Dexter" ORDER BY tv_shows.name ASC;
