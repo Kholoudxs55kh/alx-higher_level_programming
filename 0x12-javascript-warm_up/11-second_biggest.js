930fb414b0c732ec829b14cba2418d3432a92357
Kholoudxs55kh <kfattem@gmail.com>
2023-09-11 11:53:57 -0700

    handling only 2 args

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 const args = process.argv;
 const arr = args.slice(('11-second_biggest.js') + 1).sort();
 
-if (args.slice(2).length <= 2) {
+if (args.slice(2).length <= 1) {
   console.log('0');
 } else {
   console.log(Number(arr[arr.length - 2]));
