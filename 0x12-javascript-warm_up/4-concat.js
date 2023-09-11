918f60814b886db92abfdf693adbd072a8635d28
Kholoudxs55kh <kfattem@gmail.com>
2023-09-11 10:03:10 -0700

    task

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,7 +3,7 @@
 const args = process.argv.slice(2);
 const idx = args.indexOf('4-concat.js') + 1;
 const value1 = args[idx];
-const value2 = args[idx + 1]
+const value2 = args[idx + 1];
 
 if (typeof value1 !== 'undefined' && typeof value1 !== 'undefined') {
   console.log(value1 + ' is ' + value2);
