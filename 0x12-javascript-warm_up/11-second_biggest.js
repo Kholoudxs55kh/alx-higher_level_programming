c3721ea1abc04fafcf5524eb3ee5c22ef726d635
Kholoudxs55kh <kfattem@gmail.com>
2023-09-11 12:25:43 -0700

    Retreiving

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
 const args = process.argv;
-const arr = args.slice(('11-second_biggest.js') + 1);
-arr.map(Number).sort();
+const arr = args.slice(('11-second_biggest.js') + 1).sort();
 
 if (args.slice(2).length <= 1) {
   console.log('0');
