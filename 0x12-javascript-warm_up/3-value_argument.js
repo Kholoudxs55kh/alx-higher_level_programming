219515069267e3395090775b34dfc7b7596bad18
Kholoudxs55kh <kfattem@gmail.com>
2023-09-11 09:52:38 -0700

    task :3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,7 +4,7 @@ const args = process.argv.slice(2);
 const idx = args.indexOf('3-value_argument.js') + 1;
 const value = args[idx];
 
-if (idx !== 0) {
+if (typeof value !== 'undefined') {
   console.log(value);
 } else {
   console.log('No argument');
