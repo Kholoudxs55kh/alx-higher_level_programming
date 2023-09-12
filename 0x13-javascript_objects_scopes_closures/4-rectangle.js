c1ac7eec5692104dcf26da7fbd017199c72f8324
Kholoudxs55kh <kfattem@gmail.com>
2023-09-12 15:23:50 -0700

    task

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -15,13 +15,13 @@ module.exports = class Rectangle {
   }
 
   rotate () {
-    for (let i = 0; i < this.width; i++) {
-      console.log('X'.repeat(this.height));
+    for (let i = 0; i < this.width * 2; i++) {
+      console.log('X'.repeat(this.height * 2));
     }
   }
 
   double () {
-    for (let i = 0; i < this.height; i++) {
+    for (let i = 0; i < this.height * 2; i++) {
       console.log('X'.repeat(this.width * 2));
     }
   }
