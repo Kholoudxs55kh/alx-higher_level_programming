b55081f9a58919d9c4ffa69734a287ddf4ed3a56
Kholoudxs55kh <kfattem@gmail.com>
2023-09-12 16:07:02 -0700

    task

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let counter = 0;
+  Array.from(list).forEach(element => {
+    if (element === searchElement) {
+      counter +=1;
+    }
+  });
+  return counter;
+}
