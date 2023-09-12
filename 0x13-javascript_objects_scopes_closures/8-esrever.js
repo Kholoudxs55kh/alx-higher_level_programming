a713c0b77fae84ff5feb69d82508477942858643
Kholoudxs55kh <kfattem@gmail.com>
2023-09-12 16:21:16 -0700

    task

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,16 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return Array.from(list).reverse();
+  const arrList = Array.from(list);
+  let startIdx = 0;
+  let endIdx = arrList.length - 1;
+  while (startIdx < endIdx) {
+    // swapping
+    const temp = arrList[startIdx];
+    arrList[startIdx] = arrList[endIdx];
+    arrList[endIdx] = temp;
+    startIdx++;
+    endIdx--;
+  }
+  return (arrList);
 };
