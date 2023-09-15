1b355f51c39a158cadb75993fc701570bf3abb1b
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2023-09-15 20:55:21 +0300

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,20 +4,23 @@
 from sys import argv
 import MySQLdb
 
+
 def close_db(db):
     """to close all at once
     and to be reusable"""
+    curr = db.cursor()
     db.close()
-    db.cursor().close()
+    curr.close()
+
 
 if __name__ == '__main__':
 
     conn = MySQLdb.connect(
         host='localhost',
-        port = 3306,
-        user = argv[1],
-        psw = argv[2],
-        db = argv[3]
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
     )
     cur = conn.cursor()
     cur.excute('SELECT * from states')
