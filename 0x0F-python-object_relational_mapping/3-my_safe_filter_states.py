dd7244cfc837f1bc54711953d4f41009a366dd19
Kholoudxs55kh <kfattem@gmail.com>
2023-09-15 15:20:05 -0700

    task

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
 """Safe from sql injection"""
-from sys import argv
 import MySQLdb
+from sys import argv
 
 
 if __name__ == '__main__':
 
-    conn = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
-    )
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3])
+
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name=%s", (argv[4]))
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s", [argv[4]])
 
     rows = cur.fetchall()
     for rw in rows:
         print(rw)
-
     cur.close()
-    conn.close()
+    db.close()
