8033546289757398ad0819fe288588d19de3a6ea
Kholoudxs55kh <kfattem@gmail.com>
2023-09-28 19:08:04 -0700

    test

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays the size of the body of the response to the URL using Curl
-curl -Xd $1 POST "email=test@gmail.com&subject=I will always be here for PLD" -L
+curl -dsX $1 POST "email=test@gmail.com&subject=I will always be here for PLD" -L
