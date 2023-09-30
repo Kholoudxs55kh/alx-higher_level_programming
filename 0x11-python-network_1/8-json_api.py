642a7df3c75c32951dbec01ab5d15fee9e54f2ab
Kholoudxs55kh <kfattem@gmail.com>
2023-09-30 12:34:46 -0700

    test

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter."""
+if __name__ == "__main__":
+    from requests import post
+    from sys import argv
+
+    q = argv[1] if len(argv) > 1 else ""
+    res = post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        response = res.json()
+        if response == {}:
+            print('No result')
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print('Not a valid JSON')
